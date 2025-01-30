d03c556ed293361a672892f631caa6d17d84d982
zehdari <zehdari@live.com>
2025-01-30 09:41:37 -0500

    Added collision components

---
+++ Components/CollisionShape.cs
@@ -0,0 +1,16 @@
+namespace ECS.Components;
+
+public enum ShapeType
+{
+    Rectangle,
+    Line
+}
+
+public struct CollisionShape
+{
+    public ShapeType Type;
+    public Vector2 Size;       // W/H for Rectangle, End point for Line
+    public Vector2 Offset;     // Offset from position
+    public bool IsPhysical;    // Whether it should respond to collisions physically
+    public bool IsOneWay;      // For platforms you can jump through
+}
