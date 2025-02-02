d395f6a94b99d8836335ee1fa88eac68b4f9ccf0
Peter Eberhard <eberhard.47@osu.edu>
2025-02-02 09:50:57 -0500

    ActionEvent added and Raw input system set up

--- Events/ActionEvent.cs
+++ Events/ActionEvent.cs
@@ -4,7 +4,7 @@ public struct InputEvent : IEvent
 {
     public string ActionName;
     public Entity Entity;
-    public bool IsStared;
+    public bool IsStarted;
     public bool IsEnded;
     public bool IsHeld;
 }
