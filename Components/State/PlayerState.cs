7ee558add4e014421e35b4a8cec5a1c2ae2c15f5
zehdari <zehdari@live.com>
2025-02-12 04:27:58 -0500

    Fixed animations

--- Components/State/PlayerState.cs
+++ Components/State/PlayerState.cs
@@ -4,9 +4,9 @@
 public enum PlayerState
 {
     Idle = 0,       // Lowest priority
+    Fall = 20,
     Walk = 50,
     Run = 60,
-    Fall = 70,
     Jump = 80,
     Block = 90,
     Attack = 100    // Highest priority
@@ -15,5 +15,5 @@ public enum PlayerState
 // Define a component to store the player's current state
 public struct PlayerStateComponent
 {
-    public PlayerState currentState;
+    public PlayerState CurrentState;
 }
