0a534f98a4ec5ab353c4191866a6f21188373e25
zehdari <zehdari@users.noreply.github.com>
2025-02-20 06:52:18 +0000

    deploy: 470257fd5293d7fd70827b0e1e2e8d71cf0a9b2e

--- Core/EventBus.cs
+++ Core/EventBus.cs
@@ -1,14 +1,28 @@
 namespace ECS.Core;
 
+/// <summary>
+/// The event bus, a central system for managing event publication and subscription.
+/// Any subscriber to the type <typeparamref name="T"/> will recieve the published event.
+/// Each <see cref="Action"/> handles an event of type <see cref="IEvent"/>.
+/// </summary>
 public class EventBus
 {
     private Dictionary<Type, List<Action<IEvent>>> subscribers = new();
 
+    /// <summary>
+    /// Publishes an <see cref="IEvent"/> to all subscribers of that event type <typeparamref name="T"/>.
+    /// Iterates over the list of subscribers for the event type and invokes each handler with the event data.
+    /// </summary>
+    /// <typeparam name="T">The type of the event being published.</typeparam>
+    /// <param name="eventData">The data associated with the event being published.</param>
     public void Publish<T>(T eventData) where T : IEvent
     {
         var eventType = typeof(T);
+
+        // Check if there are any subscribers for the event type.
         if (subscribers.ContainsKey(eventType))
         {
+            // For each subscriber, invoke the handler with the provided event data.
             foreach (var subscriber in subscribers[eventType])
             {
                 subscriber(eventData);
@@ -16,13 +30,22 @@ public class EventBus
         }
     }
 
+    /// <summary>
+    /// Subscribes a handler to a specific event type <typeparamref name="T"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of the event being subscribed to.</typeparam>
+    /// <param name="handler">The delegate to be executed when the event is published.</param>
     public void Subscribe<T>(Action<IEvent> handler) where T : IEvent
     {
         var eventType = typeof(T);
+
+        // If no existing subscribers for this event type, initialize a new list of subscribers.
         if (!subscribers.ContainsKey(eventType))
         {
             subscribers[eventType] = new List<Action<IEvent>>();
         }
+
+        // Add the handler to the list of subscribers for the event type.
         subscribers[eventType].Add(handler);
     }
 }
