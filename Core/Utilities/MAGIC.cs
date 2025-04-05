02c7c50ae53e74fe5aced30ddd094102e4082b88
KatyaLiber1 <katyaliber2@gmail.com>
2025-04-05 03:28:23 -0400

    Made a magic class, probably eventually turned into json but this is the best I can do right now.

---
+++ Core/Utilities/MAGIC.cs
@@ -0,0 +1,15 @@
+namespace ECS.Core.Utilities;
+
+public static class MAGIC
+{
+    public static class LEVEL_KEYS
+    {   
+        public static string DAY_LEVEL { get; } = "DayLevel";
+        public static string NIGHT_LEVEL { get; } = "NightLevel";
+        public static string TEST_LEVEL { get; } = "TestLevel";
+        public static string ROOF_LEVEL { get; } = "Roof";
+        public static string DAY_LEVEL_ARENA { get; } = "DayLevelArena";
+        public static string NIGHT_LEVEL_ARENA { get; } = "NightLevelArena";
+    }
+}
+
