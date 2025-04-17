b3a8442158065ac1511e100016fa0e820e25b98f
Andy Yu <yu.3428@osu.edu>
2025-04-17 13:15:42 -0400

    Event for grabbing

---
+++ Events/GrabEvent.cs
@@ -0,0 +1,14 @@
+﻿using ECS.Components.Grab;
+namespace ECS.Events;
+
+public struct GrabEvent : IEvent
+{
+    public Entity Grabber;
+    public Entity Target;
+
+    public GrabEvent(Entity grabber, Entity target)
+    {
+        Grabber = grabber;
+        Target = target;
+    }
+}
