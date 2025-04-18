640e015de13afc341eebdbd62b1ae2cdfba69763
Andy Yu <yu.3428@osu.edu>
2025-04-17 22:05:24 -0400

    throw event after somebody grabs

---
+++ Events/ThrowEvent.cs
@@ -0,0 +1,13 @@
+﻿using ECS.Core;
+namespace ECS.Events;
+
+public struct ThrowEvent : IEvent
+{
+    public Entity Thrower;
+
+    public ThrowEvent(Entity thrower)
+    {
+        Thrower = thrower;
+    }
+}
+
